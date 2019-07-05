ced69d7222f2b2732b8cf1cb4d7dceb0a9c425cb
Lukewh <luke@lukewh.com>
2019-07-05 14:11:48 +0100

    We only need the config files

--- files/.emacs.d/elpa/which-key-20190306.1518/which-key-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "which-key" "20190306.1518" "Display available keybindings in popup" '((emacs "24.4")) :commit "c87b0ce78213f922a725be1e0ae2dede1cf29516" :authors '(("Justin Burkett" . "justin@burkett.cc")) :maintainer '("Justin Burkett" . "justin@burkett.cc") :url "https://github.com/justbur/emacs-which-key")
