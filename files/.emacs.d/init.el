28f1e7a6b808d48f6be27791efb69752099854ef
Lukewh <luke@lukewh.com>
2020-04-09 11:33:07 +0100

    General updates for the bar etc.

--- files/.emacs.d/init.el
+++ files/.emacs.d/init.el
@@ -12,7 +12,7 @@
     ("f30aded97e67a487d30f38a1ac48eddb49fdb06ac01ebeaff39439997cbdd869" "423435c7b0e6c0942f16519fa9e17793da940184a50201a4d932eafe4c94c92d" "2d1fe7c9007a5b76cea4395b0fc664d0c1cfd34bb4f1860300347cdad67fb2f9" "728eda145ad16686d4bbb8e50d540563573592013b10c3e2defc493f390f7d83" default)))
  '(package-selected-packages
    (quote
-    (expand-region flow-minor-mode vterm helm-posframe doom-theme yaml-mode which-key use-package try treemacs-projectile solarized-theme scss-mode rjsx-mode prettier-js org-bullets move-text magit json-mode helm-projectile flycheck eslintd-fix eslint-fix elpy editorconfig doom-themes doom-modeline dashboard company-tern company-quickhelp centaur-tabs buffer-move blacken add-node-modules-path))))
+    (anaconda-mode company-jedi jedi lsp-python-ms lsp-ui company-lsp lsp-mode tide ac-js2 expand-region flow-minor-mode vterm helm-posframe doom-theme yaml-mode which-key use-package try treemacs-projectile solarized-theme scss-mode rjsx-mode prettier-js org-bullets move-text magit json-mode helm-projectile flycheck eslintd-fix eslint-fix elpy editorconfig doom-themes doom-modeline dashboard company-tern company-quickhelp centaur-tabs buffer-move blacken add-node-modules-path))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
